b9eba3d0dd9b4f8d6ace947cf107b4c603345982
Arsen <zidder@hilearn.io>
2020-08-09 00:55:34 +0400

    Make pypi installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='trie-autocomplete',
+    version='0.1.0',
+    packages=['trie'],
+    author_email="hambardzumyanarsen99@gmail.com",
+    description="Autocomplete class using trie",
+    classifiers=['Programming Language :: Python :: 3',
+                 'Development Status :: 3 - Alpha']
+)
