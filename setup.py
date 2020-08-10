77e66614503014410932724fccd1300502dd1435
Arsen <zidder@hilearn.io>
2020-08-10 23:49:43 +0400

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='trie-autocomplete',
-    version='0.1.0',
+    version='0.1.1',
     packages=['trie'],
     author_email="hambardzumyanarsen99@gmail.com",
     description="Autocomplete class using trie",
